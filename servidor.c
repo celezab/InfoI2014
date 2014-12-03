b0816f654e5016eb618db93ce828ab058ac30fe1
celezab <ma.celeste.zabala@gmail.com>
2014-12-03 00:29:18 -0300

    Delete servidor.c

--- servidor.c
+++
@@ -1,9 +0,0 @@
-Para el SERVIDOR:
-
-1- Ponerlo a "escuchar" peticiones por medio de un puerto determinado.
-
-2- Cuando alguien pide conexion establecerla y unirlo al chat.
-
-3- En caso que alguno de los conectados mande algo, ver quien fue, que mando y enviarlo a todos.
-
-4- En caso de desconexion ya sea de cliente o servidor cerrar la/s conexion/es activa/s.
